5e0cae2b8383aa27de74413d799d5698bccf7bd7
Patrick Shushereba <pshushereba@gmail.com>
2019-07-18 13:21:39 -0700

    Finished MVP. Working on Stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,17 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
+           (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
+
+const testRequest = axios.get("https://api.github.com/users/pshushereba")
+  .then((axiosData) => {
+    // console.log("data", axiosData);
+    userCards.appendChild(createCard(axiosData));
+  })
+  .catch((err) => {
+    console.log("There was an error", err);
+  });
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +34,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd","bigknell"];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +55,70 @@ const followersArray = [];
 </div>
 
 */
+
+function createCard(userObj) {
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('card');
+
+  const cardImg = document.createElement('img');
+  cardImg.src = userObj.data.avatar_url;
+  cardDiv.appendChild(cardImg);
+
+  const infoDiv = document.createElement('div');
+  infoDiv.classList.add('card-info');
+    const nameHeader = document.createElement('h3');
+    nameHeader.classList.add('name');
+    nameHeader.textContent = userObj.data.name;
+    infoDiv.appendChild(nameHeader);
+
+    const pUsername = document.createElement('p');
+    pUsername.classList.add('username');
+    pUsername.textContent = userObj.data.login;
+    infoDiv.appendChild(pUsername);
+  
+    const pLocation = document.createElement('p');
+    pLocation.textContent = "Location: " + userObj.data.location;
+    infoDiv.appendChild(pLocation);
+
+    const pProfile = document.createElement('p');
+    pProfile.textContent = "Profile: ";
+      const profileLink = document.createElement('a');
+      profileLink.href = userObj.data.html_url;
+      profileLink.textContent = "View Profile";
+    pProfile.appendChild(profileLink);
+    infoDiv.appendChild(pProfile);
+
+    pFollowers = document.createElement('p');
+    pFollowers.textContent = "Followers: " + userObj.data.followers;
+    infoDiv.appendChild(pFollowers);
+
+    pFollowing = document.createElement('p');
+    pFollowing.textContent = "Following: " + userObj.data.following;
+    infoDiv.appendChild(pFollowing);
+
+    pBio = document.createElement('p');
+    pBio.textContent = "Bio: " + userObj.data.bio;
+    infoDiv.appendChild(pBio);
+  
+  cardDiv.appendChild(infoDiv);
+
+
+  return cardDiv
+}
+
+const userCards = document.querySelector('.cards');
+// console.log(userCards);
+
+followersArray.forEach((user) => {
+  axios.get(`https://api.github.com/users/${user}`)
+  .then((axiosData) => {
+    // console.log("data", axiosData);
+    userCards.appendChild(createCard(axiosData));
+  })
+  .catch((err) => {
+    console.log("There was an error", err);
+  });
+});
 
 /* List of LS Instructors Github username's: 
   tetondan
